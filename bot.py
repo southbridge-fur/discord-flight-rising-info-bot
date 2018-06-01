f8f1ea1b01f01fa67c6199ef2a796fb69fb1f890
dolphinhats <dptyler42@gmail.com>
2018-06-01 16:24:50 -0500

    removed some unrequired features

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import asyncio
 import json
 import requests
 import re
+import os
+import time
 
 client = discord.Client()
 
@@ -47,10 +49,11 @@ Genes\
     #(json.dumps(ddata,indent=4))
     return ddata
 
-def getDragonImage(dragonid):
+def getDragonImageURL(dragonid):
     id = int(dragonid)
     link = "http://flightrising.com/rendern/{2}/{0}/{1}_{2}.png".format(int((id/100)+1),id,350)
     return link
+
 
 @client.event
 async def on_ready():
@@ -73,37 +76,55 @@ async def on_message(message):
             dragonid = command[1]
             print("Parsing dragon id #{}".format(dragonid))
             ddata = lookupDragon(dragonid)
-            print("Getting dragon iamge")
-            image = getDragonImage(dragonid)
 
+            print("Getting dragon iamge")
+            imagesDir = "dergs"
+            tempFile = "/".join([imagesDir,"{0}-{1}-{2}.png".format(dragonid,ddata["data"]["name"],int(time.time()/60))])
+            if not os.path.isdir(imagesDir):
+                os.makedirs(imagesDir)
+
+            imageURL = getDragonImageURL(dragonid)
+            if not os.path.isfile(tempFile):
+                with open(tempFile, 'wb') as f:
+                    r = requests.get(imageURL, stream=True)
+                    for chunk in r.iter_content(chunk_size=1024): 
+                        if chunk:
+                            f.write(chunk)
+                            
+            with open(tempFile,"rb") as f:
+                await client.send_file(message.channel,f)
+                
+            
             print("Creating embed")
             embed = discord.Embed(title="**Dragon Profile:** #{}".format(dragonid),colour=discord.Colour(0xFFFFFF))
             
-            embed.set_image(url=image)
-            
             na = "Not Available"
 
-            embed.add_field(name="**Name**",value=ddata["data"]["name"],inline=True)
-            embed.add_field(name="**Owner**",value=na,inline=True)
-            embed.add_field(name="**Breed**",value=ddata["data"]["breed"],inline=True)
-            embed.add_field(name="**Sex**",value=ddata["data"]["sex"],inline=True)
+            embed.add_field(name="Name",value=ddata["data"]["name"],inline=True)
+            embed.add_field(name="Owner",value=na,inline=True)
+            embed.add_field(name="Breed",value=ddata["data"]["breed"],inline=True)
+            embed.add_field(name="Sex",value=ddata["data"]["sex"],inline=True)
+            embed.add_field(name="Level",value=ddata["data"]["level"],inline=True)
 
-            embed.add_field(name="**Flight**",value=na,inline=True)
-            embed.add_field(name="**Hatchday**",value=ddata["data"]["hatchday"],inline=True)
-            embed.add_field(name="**Links**",value=na,inline=True)
+            embed.add_field(name="Flight",value=na,inline=True)
+            embed.add_field(name="Hatchday",value=ddata["data"]["hatchday"],inline=True)
 
-            embed.add_field(name="**Colors and Genes**",value="""
+            embed.add_field(name="Links",value=na,inline=True)
+
+            embed.add_field(name="Colors and Genes",value="""
 **Primary:** {0[data][gene_primary]:20}
 **Secondary:** {0[data][gene_secondary]:20}
 **Tertiary:** {0[data][gene_tertiary]:20}""".format(ddata),inline=False)
 
-            embed.add_field(name="Lineage",value="{0}\n{0}\n{0}".format(na))
             
-            embed.add_field(name="Stats",value=" ".join(["**{0:left 3}**: {1:right 4} {2}".format(i,ddata["stats"][i]["base"],ddata["stats"][i]["mod"]) for i in ddata["stats"].keys()]),inline=False)
+#            stats = "{0[0]}\t{0[1]}\n{0[2]}\t{0[3]}\n{0[4]}\t{0[5]}\n{0[6]}".format(["**{0:4}**: {1:4} {2}".format(i.upper(),ddata["stats"][i]["base"],ddata["stats"][i]["mod"],align="right") for i in ddata["stats"].keys()])
+#            embed.add_field(name="Stats",value=stats,inline=False)
 
-            embed.add_field(name="Length",value=ddata["data"]["length"])
-            embed.add_field(name="Wingspan",value=ddata["data"]["wingspan"],inline=True)
-            embed.add_field(name="Weight",value=ddata["data"]["weight"],inline=True)
+            embed.add_field(name="Lineage",value="{0}\n{0}\n{0}".format(na),inline=False)
+
+#            embed.add_field(name="Length",value=ddata["data"]["length"])
+#            embed.add_field(name="Wingspan",value=ddata["data"]["wingspan"],inline=True)
+#            embed.add_field(name="Weight",value=ddata["data"]["weight"],inline=True)
             await client.send_message(message.channel, None, embed=embed)
             
 creds = json.load(open("creds.json","r"))
